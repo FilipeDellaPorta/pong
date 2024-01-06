32b4225b887a717278b78afeb706df50fc02a920
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-01-06 00:58:57 -0300

    removido a pagina about

--- js/main.js
+++ js/main.js
@@ -1,35 +1,37 @@
 function preload() {
-    trilha = loadSound("./assets/sounds/trilha.mp3");
-    ponto = loadSound("./assets/sounds/ponto.mp3");
-    raquetada = loadSound("./assets/sounds/raquetada.mp3");
+trilha = loadSound("./assets/sounds/trilha.mp3");
+ponto = loadSound("./assets/sounds/ponto.mp3");
+raquetada = loadSound("./assets/sounds/raquetada.mp3");
 }
 
 let larguraTela = 600;
 let alturaTela = 400;
 
 function setup() {
-    createCanvas (larguraTela, alturaTela);
-    //trilha.loop(); //desativado para trabalhar em paz
+createCanvas (larguraTela, alturaTela);
+//trilha.loop(); //desativado para trabalhar em paz
 }
+
 
 function draw() {
-    background(0);
-    mostraBolinha();
-    mostraRaquete(xRaquete,yRaquete);
-    mostraRaquete(xOponenteRaquete,yOponenteRaquete);
-    movimentaBolinha();
-    bolinhaPresa();
-    pararBolinha();
-    movimentaRaquete();
-    movimentaRaquetePlayer2();
-    movimentaOponenteRaqueteAI();
-    verificaBolinhaColisaoBorda();
-    //verificaColisaoRaquete();
-    verificaColisaoRaqueteBiblioteca(xRaquete,yRaquete);
-    verificaColisaoRaqueteBiblioteca(xOponenteRaquete,yOponenteRaquete);
-    mostraPlacar();
-    marcaPontos();
-    silenciaSons();
-    reativaSons();
+background(0);
+mostraBolinha();
+mostraRaquete(xRaquete,yRaquete);
+mostraRaquete(xOponenteRaquete,yOponenteRaquete);
+movimentaBolinha();
+bolinhaPresa();
+pararBolinha();
+movimentaRaquete();
+movimentaRaquetePlayer2();
+movimentaOponenteRaqueteAI();
+verificaBolinhaColisaoBorda();
+//verificaColisaoRaquete();
+verificaColisaoRaqueteBiblioteca(xRaquete,yRaquete);
+verificaColisaoRaqueteBiblioteca(xOponenteRaquete,yOponenteRaquete);
+mostraPlacar();
+marcaPontos();
+silenciaSons();
+reativaSons();
 }
 
+     
